072e7bb011dc6724d0d50ab612e8d1f6c9250b7c
MJ-Jiang <maura_jiang@outlook.com>
2025-09-02 21:59:50 +0300

    11.9

--- cypress/e2e/pokemon.cy.js
+++ cypress/e2e/pokemon.cy.js
@@ -4,4 +4,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it ('can navigate to ivysaur page and see its content', function() {
+    cy.visit('/')
+    cy.contains('ivysaur').click()
+    cy.location('pathname').should('include', '/pokemon/ivysaur')
+    cy.contains(/chlorophyll/i).should('be.visible')
+  })
 })

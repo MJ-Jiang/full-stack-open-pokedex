3d08b321581221afa06195b15040193bcb4176e4
MJ-Jiang <maura_jiang@outlook.com>
2025-09-02 23:01:27 +0300

    add health

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
 app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
+})
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
